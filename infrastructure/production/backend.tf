05d17ce3cf20f03fd0e9326f21ef7941004daaa5
ziyad-tarek1 <ziyadtarek180@gmail.com>
2025-03-29 05:14:17 +0200

    Update backend.tf

--- infrastructure/production/backend.tf
+++ infrastructure/production/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "hjvkjbkjlbjl"       
+        bucket         = "hjvkjbkjlbjl"       
     key            = "infrastructure/production/terraform.tfstate"  
     region         = "us-east-1"                             
     dynamodb_table = "terraform-lock-table"             
